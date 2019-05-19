58602d830a3162c0bf9961eefd0146bbae986e66
Omeid Eshraghi <oeshraghi@kunaiconsulting.com>
2019-05-18 18:11:49 -0700

    added a random number generator for Xkcd to show a rand img - Changed the randome generator to inside the Xkcd function, maybe this will generate a rand everytime it's called

--- plugins/xkcd/controller.js
+++ plugins/xkcd/controller.js
@@ -1,14 +1,14 @@
 function Xkcd($scope, $http, SpeechService, Focus) {
 
-
-
 	var min = 1;
 	var max = 2151;
-	var random = Math.floor((Math.random() * max) + min); 
-	console.log("random #:", random);	
 
     // Show xkcd comic
 	SpeechService.addCommand('image_comic', function () {
+
+		var random = Math.floor((Math.random() * max) + min); 
+		console.log("random #:", random);	
+
 		$http.jsonp("http://dynamic.xkcd.com/api-0/jsonp/comic/" + random + "?callback=JSON_CALLBACK")
             .then(function (response) {
 	$scope.xkcd = response.data.img;
